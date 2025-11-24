47a3d14b90efb3cb4b6e768b6d693bb4b1c66176
Paulo Lubuntu Server <phin01@gmail.com>
2025-11-24 23:41:36 +0000

    include data files reference in the estate model manifest

--- tutorials/estate/__manifest__.py
+++ tutorials/estate/__manifest__.py
@@ -4,5 +4,12 @@
         'base_setup'
     ],
 
-    'application': True
+    'application': True,
+
+    'data':[
+        'security/ir.model.access.csv',
+        
+        'views/estate_property_views.xml',
+        'views/estate_menus.xml'
+    ]
 }
