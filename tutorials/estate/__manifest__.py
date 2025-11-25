b8330a304ad9508cc3bb98535988aa6d332b725b
Paulo Lubuntu Server <phin01@gmail.com>
2025-11-25 23:15:13 +0000

    create list view for estate property

--- tutorials/estate/__manifest__.py
+++ tutorials/estate/__manifest__.py
@@ -10,6 +10,7 @@
         'security/ir.model.access.csv',
         
         'views/estate_property_views.xml',
-        'views/estate_menus.xml'
+        'views/estate_menus.xml',
+        'views/estate_list.xml'
     ]
 }
