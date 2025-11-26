8b4b91df5c6dbf63d16642d5aaac9c7b8939b9b5
Paulo Lubuntu Server <phin01@gmail.com>
2025-11-26 00:28:18 +0000

    create filters for estate list

--- tutorials/estate/__manifest__.py
+++ tutorials/estate/__manifest__.py
@@ -12,6 +12,7 @@
         'views/estate_property_views.xml',
         'views/estate_menus.xml',
         'views/estate_list.xml',
-        'views/estate_form.xml'
+        'views/estate_form.xml',
+        'views/estate_search.xml'
     ]
 }
