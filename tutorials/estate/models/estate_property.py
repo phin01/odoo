7648187ae6d8957a91517a014a6141edbf4ae410
Paulo Lubuntu Server <phin01@gmail.com>
2025-11-30 18:59:53 +0000

    feat: include compute fields in estate property

--- tutorials/estate/models/estate_property.py
+++ tutorials/estate/models/estate_property.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import fields, models, api
 
 class EstateProperty(models.Model):
     _name = "estate.property"
@@ -40,3 +40,25 @@ class EstateProperty(models.Model):
     salesperson_id = fields.Many2one("res.users", string="Salesperson", default=lambda self: self.env.user)
     property_tag_ids = fields.Many2many("estate.property.tag", string="Tags")
     property_offer_ids = fields.One2many("estate.property.offer", "property_id", string="Offers")
+    total_area = fields.Float(compute="_compute_total_area")
+    best_price = fields.Float(compute="_compute_best_price")
+
+
+    # ---------------------------------------
+    # COMPUTE METHODS
+    # ---------------------------------------
+    
+    @api.depends('living_area', 'garden_area')
+    def _compute_total_area(self):
+        for record in self:
+            record.total_area = record.living_area + record.garden_area
+
+    @api.depends('property_offer_ids')
+    def _compute_best_price(self):
+        for record in self:
+            if record.property_offer_ids:
+                record.best_price = max(record.property_offer_ids.mapped('price'))
+            else:
+                record.best_price = 0.0
+
+    
