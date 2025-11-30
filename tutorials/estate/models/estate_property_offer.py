d5c5e6478a1f6b3b571d71075fba6767f35e0cec
Paulo Lubuntu Server <phin01@gmail.com>
2025-11-30 19:04:12 +0000

    feat: include compute fields in property offer

--- tutorials/estate/models/estate_property_offer.py
+++ tutorials/estate/models/estate_property_offer.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import fields, models, api
 
 class EstatePropertyOffer(models.Model):
     _name = "estate.property.offer"
@@ -11,4 +11,29 @@ class EstatePropertyOffer(models.Model):
     ])
     partner_id = fields.Many2one("res.partner", string="Partner", required=True)
     property_id = fields.Many2one("estate.property", string="Property", required=True)
+
+    validity = fields.Integer(default=7)
+    date_deadline = fields.Date(compute="_compute_date_deadline", inverse="_inverse_date_deadline")
+
+
+    # ---------------------------------------
+    # COMPUTE METHODS
+    # ---------------------------------------
+
+    @api.depends('create_date', 'validity')
+    def _compute_date_deadline(self):
+        for record in self:
+            if record.create_date:
+                record.date_deadline = fields.Date.add(record.create_date.date(), days=record.validity)
+            else:
+                record.date_deadline = fields.Date.add(fields.Date.today(), days=record.validity)
+
+    def _inverse_date_deadline(self):
+        for record in self:
+            if record.create_date:
+                delta = (record.date_deadline - record.create_date.date()).days
+                record.validity = delta
+            else:
+                delta = (record.date_deadline - fields.Date.today()).days
+                record.validity = delta
     
