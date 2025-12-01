a9a16739dd28fcf4dfe407120786f201a3e1c1d2
Paulo Lubuntu Server <phin01@gmail.com>
2025-12-01 23:48:44 +0000

    feat: create property type form with inline property list

--- tutorials/estate/__manifest__.py
+++ tutorials/estate/__manifest__.py
@@ -16,6 +16,8 @@
         
         'views/estate_property_list.xml',
         'views/estate_property_form.xml',
-        'views/estate_property_search.xml'
+        'views/estate_property_search.xml',
+
+        'views/estate_property_type_form.xml',
     ]
 }
--- tutorials/estate/models/estate_property_type.py
+++ tutorials/estate/models/estate_property_type.py
@@ -5,6 +5,11 @@ class EstatePropertyType(models.Model):
     _description = "Real Estate Property Type"
 
     name = fields.Char(required=True)
+    property_ids = fields.One2many(
+        comodel_name='estate.property',
+        inverse_name='property_type_id',
+        string='Properties'
+    )
     
 
     # ---------------------------------------
