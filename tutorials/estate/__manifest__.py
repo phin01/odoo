79ce7352875e416b0f9930a724478479c9ba1315
Paulo Lubuntu Server <phin01@gmail.com>
2025-12-04 23:46:23 +0000

    feat: additional filters for properties

--- tutorials/estate/__manifest__.py
+++ tutorials/estate/__manifest__.py
@@ -9,14 +9,14 @@
     'data':[
         'security/ir.model.access.csv',
         
+        'views/estate_property_list.xml',
+        'views/estate_property_form.xml',
+        'views/estate_property_search.xml',
+        
         'views/estate_actions.xml',
         'views/estate_menus.xml',
 
         'views/estate_property_offer_list.xml',
-        
-        'views/estate_property_list.xml',
-        'views/estate_property_form.xml',
-        'views/estate_property_search.xml',
 
         'views/estate_property_type_list.xml',
         'views/estate_property_type_form.xml',
