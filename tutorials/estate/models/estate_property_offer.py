93383659cc5abb3880026487fda07755aa88e491
Paulo Lubuntu Server <phin01@gmail.com>
2025-12-05 01:38:08 +0000

    feat: create stat button for property type offers

--- tutorials/estate/models/estate_property_offer.py
+++ tutorials/estate/models/estate_property_offer.py
@@ -17,6 +17,8 @@ class EstatePropertyOffer(models.Model):
     validity = fields.Integer(default=7)
     date_deadline = fields.Date(compute="_compute_date_deadline", inverse="_inverse_date_deadline")
 
+    property_type_id = fields.Many2one(related="property_id.property_type_id", string="Property Type", store=True)
+
 
     # ---------------------------------------
     # CONSTRAINTS
--- tutorials/estate/models/estate_property_type.py
+++ tutorials/estate/models/estate_property_type.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import fields, models, api
 
 class EstatePropertyType(models.Model):
     _name = "estate.property.type"
@@ -12,6 +12,15 @@ class EstatePropertyType(models.Model):
         inverse_name='property_type_id',
         string='Properties'
     )
+    offer_ids = fields.One2many(
+        comodel_name='estate.property.offer',
+        inverse_name='property_type_id',
+        string='Offers'
+    )
+    offer_count = fields.Integer(
+        compute='_compute_offer_count',
+        string='Number of Offers'
+    )
     
 
     # ---------------------------------------
@@ -21,3 +30,12 @@ class EstatePropertyType(models.Model):
     _unique_name = models.Constraint(
         definition='UNIQUE(name)',
         message='Property Type already exists!')
+    
+
+    # ---------------------------------------
+    # COMPUTE METHODS
+    # ---------------------------------------
+    @api.depends('offer_ids')
+    def _compute_offer_count(self):
+        for record in self:
+            record.offer_count = len(record.offer_ids)
